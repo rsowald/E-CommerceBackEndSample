0c781efe212f22b18ea4eed0aa3505b656f4f016
rsowald <72467920+rsowald@users.noreply.github.com>
2021-03-22 23:18:54 -0700

    added category post route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,12 +29,15 @@ router.get('/:id', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-  // find one category by its `id` value
-  // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
