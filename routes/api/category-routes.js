36a579d67290ebdd8bbf605082da512a2f81e3af
rsowald <72467920+rsowald@users.noreply.github.com>
2021-03-22 23:23:16 -0700

    added category delete route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,8 +44,23 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const results = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!results) {
+      res.status(404).json({ message: `No category found with id ${req.params.id}` });
+      return;
+    }
+
+    res.status(200).json(results);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
